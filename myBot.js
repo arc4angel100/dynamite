f38e72d886e3b66294b0f4404f7eae4dcbf4efa5
Michael <minimike100@gmail.com>
2023-01-17 14:28:36 +0000

    edit my bot with random + detect gameround

--- myBot.js
+++ myBot.js
@@ -1,7 +1,32 @@
 class Bot {
+
     makeMove(gamestate) {
-        // Your code here
-    }
+
+  let player1DynamiteCount = 0;
+  let player2DynamiteCount = 0;
+  let roundNumber = 0;
+
+for (let round of gamestate.rounds) {
+if (round.p1 === "D") {
+    player1DynamiteCount++;
+}
+if (round.p2 === "D") {
+    player2DynamiteCount++;
+}
+   roundNumber ++
+}
+if (gamestate.rounds.length >= 1) {
+console.log(gamestate.rounds[gamestate.rounds.length -1].p1)
+}
+
+    let randomIndex = Math.floor(3 * Math.random())
+if ((roundNumber%12 ===0) && (player1DynamiteCount<100)){
+    return "D"
+} 
+
+else return ['R', 'P', 'S'][randomIndex];
+}
 }
 
 module.exports = new Bot();
+
